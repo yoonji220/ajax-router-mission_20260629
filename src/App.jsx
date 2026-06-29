85b35e995aafccc74be01eb05092aa61c75a4db2
choiyui <choiyj220220@gmail.com>
2026-06-29 11:27:22 +0900

    feat: implement data fetching for blog posts

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,42 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
+  useEffect(() => {
+    // let alive = true; // 상품조회 시작.... 열일 중!
+    const controller = new AbortController();
+
+    async function fetchData() {
+      try {
+        // fetch("/data/blog.json")
+        //   .then(res => res.json())
+        //   .then(result => {
+        //     if (alive) {
+        //       setPosts(result);
+        //       setLoaded(true);
+        //     }
+        //   });
+        const res = await fetch("/data/blog.json", {
+          signal: controller.signal,
+        });
+        if (!res.ok) throw new Error("메시지");
+        const data = await res.json();
+        setPosts(data);
+      } catch (e) {
+        console.error(e);
+        setPosts([]); // 로딩 중 에러 시 목록 비움
+      } finally {
+        setLoaded(true);
+      }
+    }
+    fetchData();
+
+    return () => {
+      // alive = false;
+      controller.abort();
+    }; //정리함수
+  }, []);
+  console.log(posts);
+
   return (
     <>
       <Routes>
