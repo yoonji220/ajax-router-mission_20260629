817a2ba4216b0f7dfd8b6846e1e16fcfcc409f22
choiyui <choiyj220220@gmail.com>
2026-06-29 16:49:20 +0900

    feat: update package configuration and routing setup; add gh-pages dependency and modify main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+// import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "ajax-router-mission_20260629",
+});
