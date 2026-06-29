12959d2a9691f0f9caad31885d46f83cd26bf2ce
choiyui <choiyj220220@gmail.com>
2026-06-29 17:09:11 +0900

    fix: correct base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "ajax-router-mission_20260629",
+  base: "/ajax-router-mission_20260629",
 });
